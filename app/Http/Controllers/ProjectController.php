fda8f455115d0c27ecd7e792b63df3fbb87ccbd6
stephaned <stephane.dupre@indigen.com>
2022-05-05 15:35:24 +0200

    project; tags;

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ProjectResource::collection(Project::with(['tags'])->paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $project = new Project();
+        $project->create($request->all())->tags()->attach($request->tags);
+
+        return $request;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return new ProjectResource(Project::with(['tags'])->findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $project = Project::findOrFail($id);
+        $project->save($request->all());
+        $project->tags()->sync($request->tags);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $project = Project::findOrFail($id);
+        $project->tags()->detach();
+        $project->delete();
+    }
+}
